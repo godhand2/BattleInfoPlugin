38782457c3850b572e8484b92e59e33925926e68
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2017-04-06 23:03:40 +0900

    Fix for abyssal

--- BattleInfoPlugin/Models/ShipData.cs
+++ BattleInfoPlugin/Models/ShipData.cs
@@ -507,7 +507,7 @@ namespace BattleInfoPlugin.Models
 
 			this.Condition = -1;
 
-			var isEnemyID = 500 < this.Source.Id && this.Source.Id < 901;
+			var isEnemyID = this.Source.Id > 1500;
 			var m = Plugin.RawStart2.api_mst_ship.Single(x => x.api_id == this.Source.Id);
 			this.AdditionalName = isEnemyID ? m.api_yomi : "";
 			this.TypeName = this.Source.ShipType.Name;
